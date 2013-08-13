831ceb6794cfa47950a10f47284246bb437504b7
Matt Johnson <mjohn23@gmail.com>
2013-08-13 21:19:50 +0000

    Extract XML from Board Decision

--- nlrbtoscrib.py
+++ nlrbtoscrib.py
@@ -1,5 +1,24 @@
 from urllib import urlopen # to open the RSS feeds
+import xml.etree.ElementTree as ET # to parse XML
 
+def get_case_info(item):
+    # Extracts info out of XML document
+
+    case_info = {}
+    case_info['title'] = item.find('title').text
+    description = item.find('description').text    
+    case_info['description'] = description
+    dash_pos = description.find(" - ")
+    case_num = description[:dash_pos].strip()
+    case_info['case_num'] = case_num
+    case_info['case_type'] = case_num[3:5]
+    case_info['nlrb_region'] = case_num[0:2]
+    case_info['involved_party'] = description[dash_pos+3:]
+    case_info['pub_date'] = item.find('pubDate').text
+    case_info['pdf_link'] = item.find('guid').text
+    
+    return case_info
+    
 def xml_to_string(xml_link):
     # http://docs.python.org/2/library/urllib2.html
     # Section 20.6.21 is especially helpful
@@ -7,5 +26,7 @@ def xml_to_string(xml_link):
 
 xml_link = 'http://www.nlrb.gov/rss/rssBoardDecisions.xml'
 xml_text = xml_to_string(xml_link)
+root = ET.fromstring(xml_text)
 
-
+for item in root.iter('item'):
+    print get_case_info(item)
